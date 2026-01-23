aa735cfbb4b6922fe015968250d54c4120a962ba
Ramya <241244287+ramya-2303@users.noreply.github.com>
2026-01-23 12:14:42 +0530

    Add bst_search.c to Trees folder

---
+++ Trees/bst_search.c
@@ -0,0 +1,76 @@
+// Program to search an element in a Binary Search Tree
+#include<stdio.h>
+#include<malloc.h>
+
+struct node{
+    int data;
+    struct node* left;
+    struct node* right;
+};
+
+struct node* createNode(int data){
+    struct node *n; 
+    n = (struct node *) malloc(sizeof(struct node)); 
+    n->data = data;
+    n->left = NULL; 
+    n->right = NULL; 
+    return n;
+}
+
+struct node * search(struct node* root, int key){
+    if(root==NULL){
+        return NULL;
+    }
+    if(key==root->data){
+        return root;
+    }
+    else if(key<root->data){
+        return search(root->left, key);
+    }
+    else{
+        return search(root->right, key);
+    }
+}
+
+/*
+//searching operation in an iterative way
+struct node * searchiter(struct node* root, int key){
+    while(root!=NULL){
+        if(key==root->data){
+            return root;
+        }
+        else if(key<root->data){
+            root=root->left;
+        }
+        else{
+            root=root->right;
+        }
+    }
+    return NULL;
+}
+*/
+
+int main(){
+    struct node *p = createNode(5);
+    struct node *p1 = createNode(3);
+    struct node *p2 = createNode(6);
+    struct node *p3 = createNode(1);
+    struct node *p4 = createNode(4);
+
+    p->left = p1;
+    p->right = p2;
+    p1->left = p3;
+    p1->right = p4;
+
+    struct node* n = search(p, 10);          //searchiter(p,10)
+    if(n!=NULL){
+    printf("Found: %d", n->data);
+    }
+    else{
+        printf("Element not found");
+    }
+    return 0;
+}
+
+
+
